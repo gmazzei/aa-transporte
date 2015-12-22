baa512c604bfaa7c583fed7c9523b7e5b4374b53
gmazzei <gmazzei91@gmail.com>
2015-12-22 12:45:13 -0300

    Tracing routes

--- map.js
+++ map.js
@@ -1,26 +1,120 @@
 function initMap() {
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
+    zoom: 15,
     center: {lat: -34.397, lng: 150.644}
   });
-  var geocoder = new google.maps.Geocoder();
 
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
+  var places = [];
+  var geocoder = new google.maps.Geocoder();
+  var directionsService = new google.maps.DirectionsService;
+  var service = new google.maps.DistanceMatrixService();
+
+
+  document.getElementById('add').addEventListener('click', function() {
+    geocodeAddress(geocoder, map, places);
+  });
+
+  document.getElementById('order').addEventListener('click', function() {
+    orderPlaces(places, service);
+    displayRoutes(map, directionsService, places);
   });
 }
 
-function geocodeAddress(geocoder, resultsMap) {
+function geocodeAddress(geocoder, resultsMap, places) {
   var address = document.getElementById('address').value;
+
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
+      
+      var location = results[0].geometry.location;
+
+      resultsMap.setCenter(location, 10);
+
       var marker = new google.maps.Marker({
         map: resultsMap,
-        position: results[0].geometry.location
+        position: location
       });
+
+      addPlace(places, address, location);
+      addElement(address);
+
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
   });
+}
+
+function addPlace(places, address, location) {
+  places.push({ name: address, pos: location });
+}
+
+function addElement(address) {
+  var ul = document.getElementById("list");
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode(address));
+  ul.appendChild(li);
+}
+
+
+
+function displayRoutes(map, directionsService, places) {
+  
+  for (var i = 0; i < places.length - 1; i++) {
+    displayOneRoute(map, directionsService, places[i].name, places[i+1].name);
+  }
+}
+
+
+function displayOneRoute(map, directionsService, origin, destination) {
+
+  var properties = {
+    'origin': origin,
+    'destination': destination,
+    'travelMode': google.maps.TravelMode.DRIVING
+  };
+
+  var traceRoute = function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        var directionsRenderer = new google.maps.DirectionsRenderer;
+        directionsRenderer.setMap(map);
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+  }
+
+  directionsService.route(properties, traceRoute);
+}
+
+function orderPlaces(places, service) {
+  
+  var callback = function(response, status) {
+    if (status == google.maps.DistanceMatrixStatus.OK) {
+      var origins = response.originAddresses;
+      var destinations = response.destinationAddresses;
+
+      for (var i = 0; i < origins.length; i++) {
+        var results = response.rows[i].elements;
+        for (var j = 0; j < results.length; j++) {
+          var element = results[j];
+          var distance = element.distance.text;
+          var duration = element.duration.text;
+          var from = origins[i];
+          var to = destinations[j];
+        }
+      }
+    }
+  }
+
+  var names = places.filter(function(place) {
+    return place.name;
+  });
+
+  service.getDistanceMatrix({
+    origins: names,
+    destinations: names,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, callback);
+
 }
